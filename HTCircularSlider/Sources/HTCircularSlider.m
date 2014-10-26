8c443bbd3b5acf7c504ba94f73e99f703d17ca0c
hayatan <hayatan0311@gmail.com>
2014-10-27 00:34:06 +0900

    update

--- HTCircularSlider/Sources/HTCircularSlider.m
+++ HTCircularSlider/Sources/HTCircularSlider.m
@@ -9,122 +9,151 @@
 #import "HTCircularSlider.h"
 
 @implementation HTCircularSlider {
+    CGFloat _maxRadius;
     CGPoint _origin;
     UIView *_handleView;
     UIImageView *_handleImageView;
 }
+
+#pragma mark initializer
 - (instancetype)initWithFrame:(CGRect)frame {
     if (self = [super initWithFrame:frame]) {
-        [self setup];
+        [self initialSetup];
     }
     return self;
 }
+
+#pragma mark setter
 - (void)setValue:(CGFloat)value {
     _value = value;
     [self setNeedsDisplay];
     [self sendActionsForControlEvents:UIControlEventValueChanged];
 }
+
 - (void)setRadius:(CGFloat)radius {
     _radius = radius;
-    if (_handleView) {
-        CGRect handleViewFrame = CGRectMake(_origin.x - _handleSize / 2, _origin.y - _radius  - _handleSize / 2, _handleSize, _radius * 2 + _handleSize);
-        _handleView.frame = handleViewFrame;
-    }
+    [self updateSetup];
+}
+
+- (void)setUnFillRadius:(CGFloat)unFillRadius {
+    _unFillRadius = unFillRadius;
+    [self updateSetup];
 }
 
 - (void)setHandleSize:(CGFloat)handleSize {
     _handleSize = handleSize;
+    [self updateSetup];
+}
+
+- (void)setHandlePosition:(CGFloat)handlePosition {
+    _handlePosition = handlePosition;
+    [self updateSetup];
+}
+
+- (void)setHandleImage:(UIImage *)handleImage {
+    _handleImage = handleImage;
+    [self updateSetup];
+}
+
+#pragma mark setup
+- (void)initialSetup {
+    // create subView instances
+    _handleView = [[UIView alloc] init];
+    _handleImageView = [[UIImageView alloc] init];
+    _handleImageView.contentMode = UIViewContentModeScaleToFill;
+
+    // add subViews
+    [self addSubview:_handleView];
+    [_handleView addSubview:_handleImageView];
+
+    // calc parameters
+    CGFloat w = self.frame.size.width;
+    CGFloat h = self.frame.size.height;
+    _maxRadius = w < h ? w / 2 : h / 2;
+    _origin = CGPointMake(w/2, h/2);
+
+    // set colors
+    self.insideOnly = NO;
+    self.backgroundColor = [UIColor colorWithWhite:0 alpha:0];
+    self.unFillColor = [UIColor grayColor];
+    self.fillColor = [UIColor purpleColor];
+
+    // set value params
+    self.minimumValue = 0.0;
+    self.maximumValue = 1.0;
+    self.value = 0.0;
+
+    // set appearance params
+    self.rotateAngle = 0;
+
+    // radius
+    self.radius = _maxRadius;
+    self.unFillRadius = _maxRadius;
+
+    // handle
+    self.handleSize = 20;
+    self.handlePosition = 10;
+    self.handleImage = [UIImage imageNamed:@"handle.png"];
+}
+
+- (void)updateSetup
+{
     if (_handleView) {
-        CGRect handleViewFrame = CGRectMake(_origin.x - _handleSize / 2, _origin.y - _radius  - _handleSize / 2, _handleSize, _radius * 2 + _handleSize);
+        CGRect handleViewFrame =
+                CGRectMake(_origin.x - _handleSize / 2, _origin.y - _radius  - _handleSize / 2,
+                        _handleSize, _radius * 2 + _handleSize);
         _handleView.frame = handleViewFrame;
     }
     if (_handleImageView) {
         _handleImageView.frame = CGRectMake(0, _handlePosition, _handleSize, _handleSize);
     }
-}
-
-- (void)setHandlePosition:(CGFloat)handlePosition {
-    _handlePosition = handlePosition;
     if (_handleImageView) {
-        _handleImageView.frame = CGRectMake(0, _handlePosition, _handleSize, _handleSize);
+        _handleImageView.image = _handleImage;
     }
 }
 
-- (void)setHandleImage:(UIImage *)handleImage {
-    _handleImage = handleImage;
-    if (_handleImageView) {
-        _handleImageView.image = handleImage;
+#pragma mark Touch events
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    [super touchesBegan:touches withEvent:event];
+    UITouch *touch = [touches anyObject];
+    CGPoint point = [touch locationInView:self];
+    if (!_insideOnly || [self isPointInside:point]) {
+        [self sendActionsForControlEvents:UIControlEventTouchDown];
+        self.value = [self angleFromPoint:point] / 360 * (_maximumValue - _minimumValue);
     }
 }
 
-- (void)setup {
-    self.backgroundColor = [UIColor colorWithWhite:0 alpha:0];
-    self.rotateAngle = 0;
-    self.value = 0.0;
-    self.minimumValue = 0.0;
-    self.maximumValue = 1.0;
-    self.unFillColor = [UIColor grayColor];
-    self.fillColor = [UIColor purpleColor];
-    self.handleSize = 20;
-    self.handlePosition = 10;
-
-    CGFloat w = self.frame.size.width;
-    CGFloat h = self.frame.size.height;
-    self.radius = w < h ? w / 2 : h / 2;
-    _origin = CGPointMake(w/2, h/2);
-    CGRect handleViewFrame = CGRectMake(_origin.x - _handleSize / 2, _origin.y - _radius  - _handleSize / 2, _handleSize, _radius * 2 + _handleSize);
-
-    _handleView = [[UIView alloc] initWithFrame:handleViewFrame];
-    [self addSubview:_handleView];
-
-    _handleImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, _handlePosition, _handleSize, _handleSize)];
-    _handleImageView.contentMode = UIViewContentModeScaleToFill;
-
-    [_handleView addSubview:_handleImageView];
-    self.handleImage = [UIImage imageNamed:@"handle.png"];
-
-    /**
-    * This tapGesture isn't used yet but will allow to jump to a specific location in the circle
-    */
-    UITapGestureRecognizer *tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapGestureHappened:)];
-    [self addGestureRecognizer:tapGestureRecognizer];
-
-    UIPanGestureRecognizer *panGestureRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panGestureHappened:)];
-    panGestureRecognizer.maximumNumberOfTouches = panGestureRecognizer.minimumNumberOfTouches;
-    [self addGestureRecognizer:panGestureRecognizer];
-}
-
-- (void)panGestureHappened:(UIPanGestureRecognizer *)panGestureRecognizer {
-    CGPoint point = [panGestureRecognizer locationInView:self];
-
-    switch (panGestureRecognizer.state) {
-        case UIGestureRecognizerStateChanged:
-            self.value = [self angleFromPoint:point] / 360 * (_maximumValue - _minimumValue);
-            break;
-        case UIGestureRecognizerStateEnded:
-            if ([self isPointInside:point]) {
-                [self sendActionsForControlEvents:UIControlEventTouchUpInside];
-            }
-            else {
-                [self sendActionsForControlEvents:UIControlEventTouchUpOutside];
-            }
-            break;
-        default:
-            break;
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    [super touchesBegan:touches withEvent:event];
+    UITouch *touch = [touches anyObject];
+    CGPoint point = [touch locationInView:self];
+    if ([self isPointInside:point]) {
+        [self sendActionsForControlEvents:UIControlEventTouchUpInside];
+        NSLog(@"UIControlEventTouchUpInside");
+    } else {
+        [self sendActionsForControlEvents:UIControlEventTouchUpOutside];
+        NSLog(@"UIControlEventTouchUpOutside");
     }
 }
 
-- (void)tapGestureHappened:(UITapGestureRecognizer *)tapGestureRecognizer {
-    CGPoint point = [tapGestureRecognizer locationInView:self];
+- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event {
+    [self sendActionsForControlEvents:UIControlEventTouchCancel];
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    [super touchesBegan:touches withEvent:event];
+    UITouch *touch = [touches anyObject];
+    CGPoint point = [touch locationInView:self];
     self.value = [self angleFromPoint:point] / 360 * (_maximumValue - _minimumValue);
 }
 
 - (BOOL)isPointInside:(CGPoint)point
 {
-
-    CGRect thumbTouchRect = CGRectMake(_origin.x - _radius, _origin.y - _radius, _radius*2, _radius*2);
-    return CGRectContainsPoint(thumbTouchRect, point);
+    CGFloat distance = sqrtf(powf(point.x - _origin.x, 2) + powf(point.y - _origin.y, 2));
+    return _radius >= distance;
 }
+
+#pragma mark Draw slider
 
 - (void)drawRect:(CGRect)rect {
     // Drawing code
@@ -137,16 +166,16 @@
 - (void)transformHandle
 {
     if (_handleView) {
-        _handleView.transform = CGAffineTransformMakeRotation(([self angleFromValue:_value] + _rotateAngle) * ((float)M_PI / 180));
+        _handleView.transform =
+                CGAffineTransformMakeRotation(RADIANS(([self angleFromValue:_value] + _rotateAngle)));
     }
 }
 
 - (void)drawUnFill:(CGContextRef)context
 {
-    CGContextBeginPath(context);
     [_unFillColor setFill];
-    CGContextAddArc(context, _origin.x, _origin.y, _radius, 0, 2 * (float)M_PI, 0);
-    CGContextClosePath(context);
+    CGRect borderRect = CGRectMake(_origin.x - _unFillRadius, _origin.y - _unFillRadius, _unFillRadius * 2, _unFillRadius * 2);
+    CGContextFillEllipseInRect (context, borderRect);
     CGContextFillPath(context);
 }
 
@@ -155,11 +184,14 @@
     CGContextBeginPath(context);
     [_fillColor setFill];
     CGFloat modAngle = [self angleFromValue:value] + _rotateAngle;
-    CGContextAddArc(context, _origin.x, _origin.y, _radius, RADIANS(_rotateAngle) - (float)M_PI_2, RADIANS(modAngle) - (float)M_PI_2, 0);
+    CGContextAddArc(context, _origin.x, _origin.y,
+            _radius, RADIANS(_rotateAngle) - (float)M_PI_2, RADIANS(modAngle) - (float)M_PI_2, 0);
     CGContextAddLineToPoint(context, _origin.x, _origin.y);
     CGContextClosePath(context);
     CGContextFillPath(context);
 }
+
+#pragma mark Common Utility
 
 - (CGFloat)angleFromPoint:(CGPoint)point
 {
@@ -179,9 +211,7 @@
 
 - (CGFloat)degree:(CGPoint)p1 Point2:(CGPoint)p2
 {
-    CGFloat radian = atan2f(p2.x - p1.x, p2.y - p1.y);
-    CGFloat degree = radian * 180 / (float)M_PI;
-    return degree;
+    return DEGREES(atan2f(p2.x - p1.x, p2.y - p1.y));
 }
 
 @end
--- HTCircularSlider/ViewController.m
+++ HTCircularSlider/ViewController.m
@@ -17,13 +17,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    CGRect frame = CGRectInset(self.view.bounds, 20, 100);
+    // Do any additional initialSetup after loading the view, typically from a nib.
+    CGRect frame = CGRectInset(self.view.bounds, 0, 0);
 
-    HTCircularSlider *subView = [[HTCircularSlider alloc] initWithFrame:frame];
-    [self.view addSubview:subView];
-    [subView addTarget:self action:@selector( onValueChange: ) forControlEvents:UIControlEventValueChanged ];
+    HTCircularSlider *slider = [[HTCircularSlider alloc] initWithFrame:frame];
+    [self.view addSubview:slider];
+    [slider addTarget:self action:@selector(onValueChange:) forControlEvents:UIControlEventValueChanged];
+    slider.backgroundColor = [UIColor colorWithWhite:0 alpha:0.8];
+    slider.unFillColor = [UIColor colorWithRed:0.5 green:0.5 blue:0.5 alpha:0.4];
+    slider.fillColor = [UIColor colorWithRed:0.7 green:0.3 blue:0.1 alpha:0.4];
+    slider.handleSize = 40;
+    slider.maximumValue = 72;
+    slider.value = 6;
 
+    slider.radius = frame.size.width/2 - 30;
+    slider.unFillRadius = frame.size.width/2 - 30;
 }
 
 - (void)onValueChange:(HTCircularSlider *)slider {
